e5dd3c95b603a89c63dfffd5045ab0911977337f
Josh Crawford <joshua@sgroup.com.au>
2020-04-15 09:39:40 +1000

    Add file logging helper

--- src/BaseHelper.php
+++ src/BaseHelper.php
@@ -2,6 +2,7 @@
 namespace verbb\base;
 
 use Craft;
+use craft\log\FileTarget;
 
 abstract class BaseHelper
 {
@@ -16,6 +17,33 @@ abstract class BaseHelper
             Craft::$app->setModule($moduleId, new Base($moduleId));
 
             Craft::$app->getModule($moduleId);
+        }
+    }
+
+    public static function setFileLogging($pluginHandle)
+    {
+        $hasFileLogging = false;
+
+        // Check to see if the app is using file logging.
+        foreach (Craft::$app->getLog()->targets as $target) {
+            if ($target instanceof FileTarget) {
+                $hasFileLogging = true;
+
+                break;
+            }
+        }
+
+        // If file logging is disabled, don't setup a new, separate file target. Logging will still be done
+        // it just don't be placed in a separate log file for convenience. Instead, it'll be categorised in the main log.
+        if ($hasFileLogging) {
+            Craft::getLogger()->dispatcher->targets[] = new FileTarget([
+                'logFile' => Craft::getAlias('@storage/logs/' . $pluginHandle . '.log'),
+                'categories' => [$pluginHandle],
+                'logVars' => [
+                    '_GET',
+                    '_POST',
+                ],
+            ]);
         }
     }
 }
