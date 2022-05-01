da09ad356affec86c99fadb6c6fb962d673802f5
Josh Crawford <joshua@sgroup.com.au>
2022-05-01 10:32:11 +1000

    Switch to Monolog for logging

--- src/BaseHelper.php
+++ src/BaseHelper.php
@@ -2,8 +2,10 @@
 namespace verbb\base;
 
 use Craft;
-use craft\log\FileTarget;
-use craft\web\Application;
+use craft\log\MonologTarget;
+
+use Monolog\Formatter\LineFormatter;
+use Psr\Log\LogLevel;
 
 abstract class BaseHelper
 {
@@ -23,31 +25,18 @@ abstract class BaseHelper
 
     public static function setFileLogging($pluginHandle): void
     {
-        // Prevent code from firing too early before Craft is bootstrapped
-        Craft::$app->on(Application::EVENT_INIT, function() use ($pluginHandle) {
-            $hasFileLogging = false;
-
-            // Check to see if the app is using file logging.
-            foreach (Craft::$app->getLog()->targets as $target) {
-                if ($target instanceof FileTarget) {
-                    $hasFileLogging = true;
-
-                    break;
-                }
-            }
-
-            // If file logging is disabled, don't setup a new, separate file target. Logging will still be done
-            // it just don't be placed in a separate log file for convenience. Instead, it'll be categorised in the main log.
-            if ($hasFileLogging) {
-                Craft::getLogger()->dispatcher->targets[] = new FileTarget([
-                    'logFile' => Craft::getAlias('@storage/logs/' . $pluginHandle . '.log'),
-                    'categories' => [$pluginHandle],
-                    'logVars' => [
-                        '_GET',
-                        '_POST',
-                    ],
-                ]);
-            }
-        });
+        Craft::getLogger()->dispatcher->targets[] = new MonologTarget([
+            'name' => $pluginHandle,
+            'categories' => [$pluginHandle],
+            'level' => LogLevel::INFO,
+            'allowLineBreaks' => true,
+            'maxFiles' => 10,
+            'logVars' => ['_GET', '_POST'],
+            'formatter' => new LineFormatter(
+                format: "%datetime% [%level_name%] %message%\n",
+                dateFormat: 'Y-m-d H:i:s',
+                allowInlineLineBreaks: true,
+            ),
+        ]);
     }
 }
